7f77aede4a5fbd60924fe7b3335c0d5787f753b7
Hossam Hussein <34780248+ENGHHM@users.noreply.github.com>
2018-06-12 21:12:58 +0400

    Create swe7seg.h

---
+++ 7segment_lib/Full_ATMEL_studio_tutorial/7segtest/7segtest/Debug/swe7seg.h
@@ -0,0 +1,20 @@
+/*
+ * swe7seg.h
+ *
+ * Created: /06/2018 03:25:33 ص
+ *  Author: ENG Hossam
+ */ 
+
+
+#ifndef SWE7SEG_H_
+#define SWE7SEG_H_
+
+
+
+#include <avr/io.h>							/* Include AVR std. library file */
+void send_number(char);
+
+							/* USART_RS232_H_FILE_H_ */
+
+
+#endif /* SWE7SEG_H_ */
